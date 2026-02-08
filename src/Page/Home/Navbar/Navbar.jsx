83c2340db94f80601dd41ceb868e32d7e444734a
ratulislam46 <arfanratul46@gmail.com>
2026-02-08 13:50:15 +0600

    Updated navbar

--- src/Page/Home/Navbar/Navbar.jsx
+++ src/Page/Home/Navbar/Navbar.jsx
@@ -36,45 +36,47 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                {/* large device routes link  */}
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <nav className='bg-base-100 border-b border-gray-300'>
+            <div className="navbar container mx-auto">
+                <div className="navbar-start">
+                    {/* large device routes link  */}
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {
+                                Navlinks
+                            }
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    {/* Website logo  */}
+                    <div>
+                        <ProfastIcon></ProfastIcon>
+                    </div>
+
+                </div>
+                {/* mobile device routes link  */}
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         {
                             Navlinks
                         }
                     </ul>
                 </div>
-                {/* Website logo  */}
-                <div>
-                    <ProfastIcon></ProfastIcon>
-                </div>
-
-            </div>
-            {/* mobile device routes link  */}
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                {/* login / logout button  */}
+                <div className="navbar-end">
                     {
-                        Navlinks
-                    }
-                </ul>
-            </div>
-            {/* login / logout button  */}
-            <div className="navbar-end">
-                {
-                    user ?
-                        <Link onClick={handleLogOut} to='/login' className="btn bg-green-500 text-white"> <CiLogin className='bg-green-500 text-white'/> Log out</Link> :
-                        <Link to='/login' className="btn bg-green-500 text-white"> <CiLogin/> Login</Link>
+                        user ?
+                            <Link onClick={handleLogOut} to='/login' className="btn bg-green-500 text-white"> <CiLogin className='bg-green-500 text-white' /> Log out</Link> :
+                            <Link to='/login' className="btn bg-green-500 text-white"> <CiLogin /> Login</Link>
 
-                }
+                    }
+                </div>
             </div>
-        </div>
+        </nav>
     );
 };
 
